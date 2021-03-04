37ab5c2c109401a701d9f0fbbaf8ab5287c36850
Renato Oliveira <oliversieto@gmail.com>
2021-03-03 21:31:20 -0400

    refactor: inserido o botão para adicionar uma nova tarefa

--- lib/main.dart
+++ lib/main.dart
@@ -33,40 +33,60 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var newTaskCtrl = TextEditingController();
+
+  void add() {
+    if (newTaskCtrl.text.isEmpty) {
+      return;
+    }
+
+    setState(() {
+      widget.items.add(
+        Item(title: newTaskCtrl.text, done: false),
+      );
+      newTaskCtrl.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: TextFormField(
-            controller: newTaskCtrl,
-            keyboardType: TextInputType.text,
-            style: TextStyle(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+          decoration: InputDecoration(
+            labelText: 'Nova Tarefa',
+            labelStyle: TextStyle(
               color: Colors.white,
-              fontSize: 18,
-            ),
-            decoration: InputDecoration(
-              labelText: 'Nova Tarefa',
-              labelStyle: TextStyle(
-                color: Colors.white,
-              ),
             ),
           ),
         ),
-        body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            final item = widget.items[index];
-            return CheckboxListTile(
-              value: item.done,
-              onChanged: (value) {
-                setState(() {
-                  item.done = value;
-                });
-              },
-              title: Text(item.title),
-              key: Key(item.title),
-            );
-          },
-          itemCount: widget.items.length,
-        ));
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          final item = widget.items[index];
+          return CheckboxListTile(
+            value: item.done,
+            onChanged: (value) {
+              setState(() {
+                item.done = value;
+              });
+            },
+            title: Text(item.title),
+            key: Key(item.title),
+          );
+        },
+        itemCount: widget.items.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
+      ),
+    );
   }
 }
